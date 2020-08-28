58ca2461348322eee4cf0ef31d8694761bff22ac
Amrhitzz <amrhitzz@gmail.com>
2020-08-28 19:40:17 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component{
+
+  state = {
+    value: 1,
+    message: "increase or decrease",
+    showMessage: true
+  }
+
+
+
+  handleIncrement = () => {
+    this.setState({
+      value: this.state.value + 1,
+      showMessage: false
+    })
+  }
+
+  decrement = () => {
+    if (this.state.value <= 0){
+      this.setState({
+        message: "Value can't be lesser than 0",
+        showMessage: true
+      })
+    }else{
+      this.setState({
+        value: this.state.value - 1
+      })
+    }
+  }
+
+  reset = () => {
+    this.setState({
+      value: this.state.value = 0,
+      message:"increase or decrease",
+      showMessage:true
+    })
+  }
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <h1>Hello World</h1>
+      <h1>{this.state.value}</h1>
+      <button onClick = {()=> this.handleIncrement()}>Increment</button>
+      <button onClick = {()=> this.reset()}>Reset</button>
+      <button onClick = {()=> this.decrement()}>Decrement</button>
+      {this.state.showMessage === true &&
+      <p style={{color: 'red'}}>{this.state.message}</p> 
+      }
     </div>
   );
+  }
 }
 
 export default App;
